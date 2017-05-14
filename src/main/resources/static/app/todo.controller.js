91fcedd6002e279d1b3e83275d755824640438d2
James Novak <jamesnovak90@gmail.com>
2017-05-14 18:21:37 -0400

    add angular todo controller

---
+++ src/main/resources/static/app/todo.controller.js
@@ -0,0 +1,13 @@
+(function(){
+  'use strict';
+  function TodoController() {
+    var vm = this;
+  }
+  
+
+
+
+  angular
+  .module('app')
+  .controller('TodoController', TodoController);
+})();
